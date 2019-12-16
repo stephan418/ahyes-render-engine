df845870b2e9d70381320558a31ea13a4f2db58a
Stefan <32109571+Stefan@users.noreply.github.com>
2019-12-16 16:39:55 +0100

    Added Window Class

---
+++ ahyes-core/Window.cpp
@@ -0,0 +1,18 @@
+#include "Window.h"
+#include <iostream>
+
+using namespace std;
+
+void Window::init()
+{
+	cout << "ich bin so SUPER!!!";
+}
+
+Window::Window() {
+	init();
+}
+
+
+Window::~Window()
+{
+}
---
+++ ahyes-core/Window.h
@@ -0,0 +1,12 @@
+#pragma once
+class Window
+{
+private:
+	int cool = 0;
+public:
+	Window();
+	~Window();
+private:
+	void init();
+};
+
